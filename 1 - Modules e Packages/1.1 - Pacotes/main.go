cf3a22bd1298e66ff1a4411c617067114709194a
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-02-13 23:39:28 -0300

    learning about packages and initializing a go file

---
+++ 1 - Modules e Packages/1.1 - Pacotes/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "meu_projeto/mathutils"
+)
+
+func main() {
+    resultado := mathutils.Add(5, 10)
+    fmt.Println("Resultado da soma:", resultado)
+}
---
+++ 1 - Modules e Packages/1.1 - Pacotes/mathutils/math.go
@@ -0,0 +1,6 @@
+package mathutils
+
+// Função para somar dois números e retornar o resultado
+func Add(a int, b int) int {
+	return a + b
+}
